2f1ba1028be44a1851272496d142eafa6df58816
Uemit Seren <uemit.seren@gmi.oeaw.ac.at>
2010-08-25 17:09:09 +0200

    Removed unnecessary imports

--- GWASWebClient/src/edu/nordborglab/client/Comparison.java
+++ GWASWebClient/src/edu/nordborglab/client/Comparison.java
@@ -2,13 +2,9 @@ package edu.nordborglab.client;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.dom.client.Style.Unit;
-import com.google.gwt.user.client.ui.Composite;
-import com.google.gwt.user.client.ui.HTMLPanel;
-import com.google.gwt.user.client.ui.LayoutPanel;
 import com.google.gwt.user.client.ui.RootLayoutPanel;
 import com.google.gwt.user.client.ui.TabLayoutPanel;
-import com.google.gwt.user.client.ui.Widget;
+
 import com.google.gwt.uibinder.client.UiBinder;
 
 public class Comparison implements EntryPoint {
